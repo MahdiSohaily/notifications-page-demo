3eb8920a92cb12891b98b0a781513eedc4579d0d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-26 11:35:32 +0430

    Add event listener to mark unread as read for individual notification

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,13 @@ const markRead = document.querySelector('.read');
 const unread = document.querySelectorAll('.active');
 
 markRead.addEventListener('click', () => {
-    unread.forEach( elem => {
-        elem.classList.remove('active');
-    }
-})
+  unread.forEach((elem) => {
+    elem.classList.remove('active');
+  });
+});
+
+unread.forEach((elem) => {
+  elem.addEventListener('click', (e) => {
+    e.target.classList.remove('active')
+  });
+});
